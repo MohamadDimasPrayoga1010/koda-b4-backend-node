3bcf149674afb4336dd80355f0a9e98b6d6f0d46
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-28 22:04:26 +0700

    update prisma use posgress

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,11 +1,13 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
 import { env } from "node:process";
 import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+// const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaPg({connectionString})
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
