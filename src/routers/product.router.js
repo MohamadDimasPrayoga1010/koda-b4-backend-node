a6e9f201b9e766b3698e3aaa1eb911784f1c6e74
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-28 22:01:46 +0700

    feat: add multer handle response

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -4,13 +4,14 @@ import { createProductController, deleteProductController, getAllProductsControl
 import { upload } from "../libs/uploads.js";
 import { validate } from "../libs/validate.js";
 import { createProductValidation, updateProductValidation } from "../validator/productValidator.js";
+import { multerErrorHandler } from "../libs/multerHandler.js";
 
 const router = express.Router();
 
-router.post("", upload.array("images", 4), authMiddleware,validate,createProductValidation, adminOnly, createProductController);
-router.post("/:id/upload", upload.array("images", 5), uploadProductImageController)
+router.post("", upload.array("images", 4), authMiddleware,validate,createProductValidation, multerErrorHandler ,adminOnly, createProductController);
+router.post("/:id/upload", upload.array("images", 5), multerErrorHandler,uploadProductImageController)
 router.get("", authMiddleware, getAllProductsController);
 router.get("/:id", authMiddleware, getProductByIdController);
-router.patch("/:id", authMiddleware,validate,upload.array("images", 5) ,updateProductValidation, adminOnly, updateProductController);
+router.patch("/:id", authMiddleware,validate,upload.array("images", 5) ,updateProductValidation, adminOnly, multerErrorHandler,updateProductController);
 router.delete("/:id", authMiddleware, adminOnly, deleteProductController)
 export default router;
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,6 +2,7 @@ import express from "express";
 import { adminOnly, authMiddleware } from "../middlewares/authMiddleware.js";
 import { addUserController, deleteUserController, editUserController, getAllUsersController, getUserByIdController } from "../controllers/user.controllers.js";
 import { upload } from "../libs/uploads.js";
+import { multerErrorHandler } from "../libs/multerHandler.js";
 
 
 
@@ -10,8 +11,8 @@ const router = express.Router();
 
 router.get("/", authMiddleware, adminOnly, getAllUsersController)
 router.get("/:id", authMiddleware, adminOnly, getUserByIdController)
-router.post("/", upload.single("image"), authMiddleware, adminOnly, addUserController)
-router.patch("/:id", authMiddleware, upload.single("image"), editUserController, adminOnly);
+router.post("/", upload.single("image"), authMiddleware, multerErrorHandler ,adminOnly, addUserController)
+router.patch("/:id", authMiddleware, upload.single("image"),multerErrorHandler ,editUserController, adminOnly);
 router.delete("/:id", authMiddleware, adminOnly, deleteUserController);
 export default router;
 
