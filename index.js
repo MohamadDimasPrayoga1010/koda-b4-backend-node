01df08bc716dd8b5ca46f32d0cd07d58fb4772ff
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-30 05:10:29 +0700

    feat: add router

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import authRoutes from "./src/routers/auth.router.js";
-// import productRouter from "./src/routers/product.router.js"
-import { initDocs } from "./src/libs/initDocs.js";
+import productRouter from "./src/routers/product.router.js"
+import userRouter from "./src/routers/user.router.js"
+import orderRouter from "./src/routers/order.router.js"
 import path from "node:path"
 import 'dotenv/config';
 import { authMiddleware } from "./src/middlewares/authMiddleware.js";
+import { initDocs } from "./src/libs/initDocs.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
-// app.use("/api", authMiddleware, productRouter)
+app.use("/products", productRouter)
+app.use("/users", userRouter)
+app.use("/", orderRouter)
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 initDocs(app)
