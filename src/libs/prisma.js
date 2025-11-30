310858df0d104de2e0156c2102f3a625432f3b11
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-01 01:38:17 +0700

    set up deploy

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,11 +1,11 @@
-import pkg from "../generated/prisma/index.js";
-const { PrismaClient } = pkg;
-import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
 
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
