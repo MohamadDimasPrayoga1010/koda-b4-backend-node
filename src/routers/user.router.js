2aebd3a3e82832f351e85d1d674f0fc34c7a3a71
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-07 00:16:54 +0700

    feat: add router validate

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,20 +1,56 @@
 import express from "express";
 import { adminOnly, authMiddleware } from "../middlewares/authMiddleware.js";
-import { addUserController, deleteUserController, editUserController, getAllUsersController, getUserByIdController, getUserProfileController, updateUserProfileController } from "../controllers/user.controllers.js";
+import {
+  addUserController,
+  deleteUserController,
+  editUserController,
+  getAllUsersController,
+  getUserByIdController,
+  getUserProfileController,
+  updateUserProfileController,
+} from "../controllers/user.controllers.js";
 import { upload } from "../libs/uploads.js";
 import { multerErrorHandler } from "../libs/multerHandler.js";
-
-
+import {
+  addUserValidation,
+  editUserValidation,
+} from "../validator/userValidator.js";
+import { validate } from "../libs/validate.js";
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, getUserProfileController)
-router.patch("/profile", authMiddleware, upload.single("image") ,updateUserProfileController,multerErrorHandler)
-router.get("/", authMiddleware, adminOnly, getAllUsersController)
-router.get("/:id", authMiddleware, adminOnly, getUserByIdController)
-router.post("/", upload.single("image"), authMiddleware, multerErrorHandler ,adminOnly, addUserController)
-router.patch("/:id", authMiddleware, upload.single("image"),multerErrorHandler ,editUserController, adminOnly);
-router.delete("/:id", authMiddleware, adminOnly, deleteUserController);
+router.get("/profile", authMiddleware, getUserProfileController);
+router.patch(
+  "/profile",
+  authMiddleware,
+  upload.single("image"),
+  updateUserProfileController,
+  multerErrorHandler
+);
+router.get("/", authMiddleware, adminOnly, getAllUsersController);
+router.get("/:id", authMiddleware, adminOnly, getUserByIdController);
+router.post(
+  "/",
+  authMiddleware,
+  adminOnly,
+  upload.single("image"),
+  multerErrorHandler,
+  addUserValidation,
+  validate,
+  addUserController
+);
 
+router.patch(
+  "/:id",
+  authMiddleware,
+  adminOnly,
+  upload.single("image"),
+  multerErrorHandler,
+  editUserValidation,
+  validate,
+  editUserController
+);
+
+router.delete("/:id", authMiddleware, adminOnly, deleteUserController);
 
 export default router;
