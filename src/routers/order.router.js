7def18d38e43a379125a67dd58c13f8da672b0e7
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-07 00:16:04 +0700

    fix typo

--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/cart", authMiddleware, addToCart)
 router.get("/cart", authMiddleware, getCart)
 router.delete("/cart/:id", authMiddleware, deleteCart)
-router.post("/transactions", authMiddleware, createTransactionController)
+router.post("/transactions", authMiddleware,createTransactionController)
 router.get("/transactions/history", authMiddleware, getTransactionHistoryController)
 router.get("/transactions/history/:id", authMiddleware, getTransactionDetail)
 
